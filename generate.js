144854d9c5c7d6871d9e6823822c4e7bc9e7bd1a
张博 <zhangbo@panda.tv>
2018-10-24 21:30:58 +0800

    Signed-off-by: 张博 <zhangbo@panda.tv>

--- generate.js
+++ generate.js
@@ -2,31 +2,77 @@ const fs = require("fs");
 const { Transform } = require("stream");
 const Sax = require("sax");
 
-// const saxStream = Sax.createStream(true);
 
-// saxStream.on("opentag", tag => {
-//   console.log("tag", tag);
-// });
+const svgpath = require("svgpath");
 
-// saxStream.on("end", () => {
-//   console.log("end");
-// });
-
-// fs.createReadStream("./svg/all.svg").pipe(saxStream);
 
 async function generate(src) {
-
   const saxStream = Sax.createStream(true);
+  // const transformStack = [new Matrix()];
+  // function applyTransform(d) {
+  //   return new SVGPathData(d).matrix(
+  //     ...transformStack[transformStack.length - 1].toArray()
+  //   );
+  // }
 
-  let glyph=[];
+  let glyph = {
+    width: 0,
+    height: 0,
+    paths: []
+  };
+
+  let vb = [];
 
   return new Promise((resolve, reject) => {
+
     saxStream.on("opentag", tag => {
-      console.log("tag", tag);
-      glyph.push(tag);
+      if (tag.name === "svg") {
+        if ("viewBox" in tag.attributes) {
+          vb = tag.attributes.viewBox.split(/\s*,*\s|\s,*\s*|,/);
+          const dx = parseFloat(vb[0]);
+          const dy = parseFloat(vb[1]);
+          const width = parseFloat(vb[2]);
+          const height = parseFloat(vb[3]);
+          glyph.width =
+            "width" in tag.attributes
+              ? parseFloat(tag.attributes.width)
+              : width;
+          glyph.height =
+            "height" in tag.attributes
+              ? parseFloat(tag.attributes.height)
+              : height;
+
+          // transformStack[transformStack.length - 1]
+          //   .translate(-dx, -dy)
+          //   .scale(glyph.width / width, glyph.height / height);
+        } else {
+          glyph.width = parseFloat(tag.attributes.width);
+          glyph.height = parseFloat(tag.attributes.height);
+        }
+      }
+      if (tag.name === "path" && tag.attributes.d) {
+        // glyph.paths.push(applyTransform(tag.attributes.d));
+        glyph.d = svgpath(tag.attributes.d)
+          .translate(-parseFloat(vb[0]), -parseFloat(vb[1]))
+          .scale(
+            glyph.width / parseFloat(vb[2]),
+            glyph.height / parseFloat(vb[3])
+          )
+          .rel()
+          .round(1)
+          .toString();
+      }
     });
 
     saxStream.on("end", () => {
+      // const glyphPath = new SVGPathData("");
+      // const glyphPathTransform = new Matrix().transform(1, 0, 0, -1, 0, 0); // ySymmetry
+      // glyph.paths.forEach(path => {
+      //   glyphPath.commands.push(
+      //     ...path.toAbs().matrix(...glyphPathTransform.toArray()).commands
+      //   );
+      // });
+      // glyph.d =  glyphPath.round(1000).encode();
       resolve(glyph);
     });
 
@@ -34,7 +80,6 @@ async function generate(src) {
   });
 }
 
-
-generate().then(data=>{
-    console.log(data);
-})
+generate().then(data => {
+  console.log(data);
+});
